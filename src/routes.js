9ae630e741ccbc2b6b2e7885025c0ffb31040b80
maxgarcia123 <maxg.silva123@gmail.com>
2020-11-23 00:44:56 -0300

    creation routes and data base config

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+const { Router } = require('express');
+
+const UserController =  require('./controllers/UserController')
+const FinalDriverController =  require('./controllers/FinalDriverController')
+const CardController =  require('./controllers/CardController')
+const ConductorDriverController =  require('./controllers/ConductorDriverController')
+
+
+const routes = Router();
+
+routes.get('/users', UserController.index);
+routes.get('/users/:email/:password', UserController.getUser);
+routes.post('/users', UserController.store);
+routes.delete('/users/:user_id', UserController.deleteUser)
+
+routes.post('/finalDrivers', FinalDriverController.store);
+routes.get('/finalDrivers', FinalDriverController.index);
+
+
+routes.post('/conductorDrivers', ConductorDriverController.store)
+
+routes.post('/users/:user_id/card', CardController.store);
+
+
+module.exports = routes;
